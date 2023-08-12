1ae2fd81f1e24dd9e05eed8cd0b4a71382ba6005
pongchul <inchul7511@gmail.com>
2023-08-13 00:01:16 +0900

    Featuring : Practice React and TypeScript !

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [user, setUser] = useState({
+    id: 1,
+    name: "pongchul",
+    email: "asdfg12@google.com",
+    level: 10, // There are 1 to 10 and 1 is the best.
+    gender: "MALE",
+    age: 29,
+    address: "",
+  });
+
+  const edit = () => {
+    setUser({ ...user, name: "poooongchul" });
+  };
+
+  const login = () => {};
+
   return (
     <div>
-      <input type={"text"} />
+      <div>id: {user.id}</div>
+      <div>name: {user.name}</div>
+      <div>email: {user.email}</div>
+      <div>level: {user.level}</div>
+      <div>gender: {user.gender}</div>
+      <div>age: {user.age}</div>
+
+      {user.age >= 30 ? (
+        <div style={{ backgroundColor: "blue" }}> age: {user.age}</div>
+      ) : (
+        <div style={{ backgroundColor: "black" }}> age : {user.age}</div>
+      )}
+      <button onClick={() => edit()}> 수정 </button>
     </div>
   );
 }
